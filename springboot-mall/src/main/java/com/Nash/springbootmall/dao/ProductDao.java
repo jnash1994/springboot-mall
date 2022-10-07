d54fd88f47d08968d106b0a0353900549474876f
jnash1994 <bigearblack@gmail.com>
2022-10-07 17:34:59 +0800

    商品功能 - 實作新增商品

--- springboot-mall/src/main/java/com/Nash/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/Nash/springbootmall/dao/ProductDao.java
@@ -1,7 +1,9 @@
 package com.Nash.springbootmall.dao;
 
+import com.Nash.springbootmall.dto.ProductRequest;
 import com.Nash.springbootmall.model.Product;
 
 public interface ProductDao {
     Product getProductById(Integer productId);
+    Integer createProduct(ProductRequest productRequest);
 }
--- springboot-mall/src/main/java/com/Nash/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/Nash/springbootmall/service/ProductService.java
@@ -1,7 +1,9 @@
 package com.Nash.springbootmall.service;
 
+import com.Nash.springbootmall.dto.ProductRequest;
 import com.Nash.springbootmall.model.Product;
 
 public interface ProductService {
     Product getProductById(Integer productId);
+    Integer createProduct(ProductRequest productRequest);
 }
