d71ee022c46d7bfe87f4a88c8d43df39a7aa6eb8
jnash1994 <bigearblack@gmail.com>
2022-10-08 10:58:44 +0800

    商品功能 -實作查詢商品列表(基礎實作)

--- springboot-mall/src/main/java/com/Nash/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/Nash/springbootmall/dao/ProductDao.java
@@ -3,7 +3,10 @@ package com.Nash.springbootmall.dao;
 import com.Nash.springbootmall.dto.ProductRequest;
 import com.Nash.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
