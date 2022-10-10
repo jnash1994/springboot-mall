fccc2073aaffe383c2d18a48a17806d22c5c1129
jnash1994 <bigearblack@gmail.com>
2022-10-10 22:31:27 +0800

    訂單功能 - 查詢訂單列表

--- springboot-mall/src/main/java/com/Nash/springbootmall/dao/OrderDao.java
+++ springboot-mall/src/main/java/com/Nash/springbootmall/dao/OrderDao.java
@@ -1,11 +1,14 @@
 package com.Nash.springbootmall.dao;
 
+import com.Nash.springbootmall.dto.OrderQueryParams;
 import com.Nash.springbootmall.model.Order;
 import com.Nash.springbootmall.model.OrderItem;
 
 import java.util.List;
 
 public interface OrderDao {
+    Integer countOrder(OrderQueryParams orderQueryParams);
+    List<Order> getOrders(OrderQueryParams orderQueryParams);
     Order getOrderById(Integer orderId);
     List<OrderItem> getOrderItemsByOrderId(Integer orderId);
      Integer createOrder(Integer userId,Integer totalAmount);
--- springboot-mall/src/main/java/com/Nash/springbootmall/service/OrderService.java
+++ springboot-mall/src/main/java/com/Nash/springbootmall/service/OrderService.java
@@ -1,9 +1,16 @@
 package com.Nash.springbootmall.service;
 
 import com.Nash.springbootmall.dto.CreateOrderRequest;
+import com.Nash.springbootmall.dto.OrderQueryParams;
 import com.Nash.springbootmall.model.Order;
 
+import java.util.List;
+
 public interface OrderService {
+
+    Integer countOrder (OrderQueryParams orderQueryParams);
+    List<Order> getOrders(OrderQueryParams orderQueryParams);
+
     Order getOrderById(Integer orderId);
     Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest);
 
